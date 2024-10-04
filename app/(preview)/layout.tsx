dcb97cbe028a9c75bc733face4e52ec0b8edd9a2
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-04 18:24:56 -0400

    x

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://ai-sdk-preview-attachments.vercel.dev"),
-  title: "Attachments Preview",
+  title: "Da Braidr by Minne",
   description: "Experimental preview of attachments in useChat hook",
 };
 
@@ -15,6 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+      />
       <body>
         <Toaster position="top-center" richColors />
         {children}
