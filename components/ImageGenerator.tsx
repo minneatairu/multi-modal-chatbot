770ebbdafc1be36ced468f8546e38329d683287b
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-07 20:14:13 -0400

    m

--- components/ImageGenerator.tsx
+++ components/ImageGenerator.tsx
@@ -64,7 +64,7 @@ export default function ImageGenerator() {
   };
 
   return (
-    <div className="container max-w-2xl mx-auto p-5">
+    <div className="container">
 
 
       <form className="image-submit" onSubmit={handleSubmit}>
