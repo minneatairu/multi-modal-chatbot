5512d6ffae5ffb83acd0d10b0449c0c474bbd183
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-08 14:03:28 -0400

    d

--- components/EssayContent.tsx
+++ components/EssayContent.tsx
@@ -20,76 +20,17 @@ const EssayContent: React.FC = () => {
             </p>
 
             <p className="body-text">
+            Often described as an art form, hair braiding is, however, driven by ethnomathematical principles. Translation determines the spacing between the rows, tessellation forms geometric patterns, rotation curves the braid’s path, reflection ensures symmetry, and dilation controls expansion/contraction.
             </p>
             <p className="body-text">
-                The art of braiding cannot be divorced from the Black struggle for liberation. West African women, in anticipation for the horrors of the Middle Passage,
-                wove life-sustaining rice, okra, and other seeds into their
-                cornrows. Others braided gold and small valuables
-                within their cornrows hoping to preserve some wealth even in dire circumstances.
-                Enslaved Afro-Colombian women, too, braided maps and messages of
-                escape into intricate cornrows. The art of braiding is an intergenerational practice guided by intuitive ethnomathematical concepts.
-                Translation determines the spacing between the rows,
-                tessellation forms geometric patterns, rotation curves the
-                braid’s path, reflection ensures symmetry, and dilation controls
-                expansion/contraction.
+            Though the 2019 U.S. CROWN Act prohibits hair-based discrimination, transactions between the braider and the braided remain tethered to the enduring weight of Eurocentric beauty standards. What might appear as a simple grooming choice is, in fact, a decision with far-reaching consequences for one’s economic and social mobility. As Joseph-Salisbury & Connelly (2018) remind us: "If your hair is relaxed, white people are relaxed. If your hair is nappy, they're not happy". Which braid will pass the test of the White gaze? Will it conform to school dress codes or be condemned as "unkempt," "extreme...faddish and out of control"? Is it deemed appropriate in professional settings, for a job interview, or to ensure job security and advancement—or will it foreclose those opportunities? Can it be modified to minimize its association with "criminality" or the stereotype of "the angry Black woman"? Is it deemed "too ghetto" or "too ethnic"? Too "Afrocentric"? Can a particular braid pattern ensure quicker passage through TSA checkpoints? How will healthcare providers perceive it—unhygienic or unclean? Will the braid, ultimately, affect the quality of care a patient receives?
             </p>
-            <p className="body-text">
-                Currently valued at $529.3 million, and projected to grow to
-                $625.3 million by 2032, the Black hair braiding industry is a
-                low-cost, high-return ecosystem. This economy, primarily driven
-                by Black women entrepreneurs and sustained by the preferences of
-                Black femme consumers thrives despite operating within "the
-                bounds of the white supremacist patriarchal state,
-                macrocosmically, and the Black male head of the patriarchal
-                family, microcosmically." Hair discrimination—that
-                all-too-familiar form of racial bias characterized by the
-                hyperregulation and hypersurveillance of Black hair
-                textures—plays a critical role in shaping both consumer demand
-                and pricing models. Lightweight, non-voluminous braids made with
-                human-like synthetic hair, often prized for their semblance to
-                non-coily textures, command a median price of $250, double the
-                cost of Afrocentric styles braided with kanekalon, a coarse,
-                coily synthetic fiber.
-            </p>
+      
+
+            
 
             <p className="body-text">
-                Though the 2019 U.S. CROWN Act prohibits hair-based
-                discrimination, the transactions between the braider and the
-                braided remain tethered to the enduring weight of Eurocentric
-                beauty standards. What might appear as a simple grooming choice
-                is, in fact, a decision with far-reaching consequences for one’s
-                economic and social mobility. As Joseph-Salisbury & Connelly
-                (2018) remind us: "If your hair is relaxed, white people are
-                relaxed. If your hair is nappy, they're not happy". Which braid
-                will pass the test of the White gaze? Will it conform to school
-                dress codes or be condemned as "unkempt," "extreme...faddish and
-                out of control"? Is it deemed appropriate in professional
-                settings, for a job interview, or to ensure job security and
-                advancement—or will it foreclose those opportunities? Can it be
-                modified to minimize its association with "criminality" or the
-                stereotype of "the angry Black woman"? Is it deemed "too ghetto"
-                or "too ethnic"? Too "Afrocentric"? Can a particular braid
-                pattern ensure quicker passage through TSA checkpoints? How will
-                healthcare providers perceive it—unhygienic or unclean? Will the
-                braid, ultimately, affect the quality of care a patient
-                receives?
-            </p>
-
-            <p className="body-text">
-                What happens when these critical decisions between the braider
-                and the braided are replaced by generative systems that
-                abstract, optimize, and commodify them? What happens when the
-                labor of hair braiding, long undervalued and overlooked, is
-                severed from the expertise of Black women who have sustained it
-                for centuries? Can such a system meaningfully model braiding
-                traditions nurtured in backyards, courtyards, front porches, and
-                stoops? And to what extent can a system serve the diverse
-                grooming needs of Black femme consumers-from hairstyle
-                selection, braider identification, synthetic fiber sourcing,
-                hairstyle maintenance, hair product reviews, to recommendations?
-                What happens when such a system, designed in the pursuit of
-                capital, is planted within Black communities and harvested for
-                profit?
+            What happens when these critical decisions between the braider and the braided are replaced by generative systems? To what extent might an image generation replace the ritual of doomscrolling Instagram for braid inspirations? And, to what extent can conversational agents exploit and commodify the psychohairapeutic exchanges between braiders and clients—exchanges rooted in the shared navigation of a world that polices Black hair? Can such a system meaningfully model braiding traditions nurtured in backyards, courtyards, front porches, and stoops? 
             </p>
 
         </div>
