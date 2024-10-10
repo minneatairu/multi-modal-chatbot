9a01891ca474ecd1e45f3d44d0af935fe756cee6
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-09 22:55:57 -0400

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "replicate.com",
-        },
-        {
-          protocol: "https",
-          hostname: "replicate.delivery",
-        },
-        {
-          protocol: "https",
-          hostname: "*.replicate.delivery",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
