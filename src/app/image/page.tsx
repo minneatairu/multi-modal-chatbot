35b754f1cab1784301ff5167397870dfcd975945
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-11 02:34:41 -0400

    n

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
-      <h2>Welcome to the About Page</h2>
-      <p>This is where we talk about us.</p>
+  Welcome to the About Page
+This is where we talk about us.
     </div>
   );
 };
