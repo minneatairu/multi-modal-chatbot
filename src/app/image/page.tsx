eb3157b199a894e12dbde31f3e6e2cbdbff45d85
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-11 02:32:13 -0400

    n

---
+++ src/app/image/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>Welcome to the About Page</h2>
+      <p>This is where we talk about us.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
