1c4b6a4b83a14ef9b1d0f4c2a7a4c4579d3de451
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-11 02:48:07 -0400

    h

--- src/app/braiding/page.tsx
+++ src/app/braiding/page.tsx
@@ -4,42 +4,7 @@ import ImageGenerator from "@/components/ImageGenerator";
 const AboutPage = () => {
   return (
     <div>
-   <form
-            className="text-prompt-form expanded"
-            onSubmit={handleFormSubmit}
-            style={{
-              backgroundColor: "black",
-              color: "white",
-              position: "relative",
-              border: "none",
-            }}
-            ref={formRef}
-          >
-            <div className="animated-text-container">
-              <div className="animated-text">
-                {words.map((word, index) =>
-                  word === "<br>" ? (
-                    <br key={index} style={{ marginBottom: "20px" }} />
-                  ) : (
-                    <span
-                      key={index}
-                      style={{ animationDelay: `${index * 0.2}s` }}
-                    >
-                      {word}&nbsp;
-                    </span>
-                  )
-                )}
-              </div>
-              <div>
       <ImageGenerator />
-    </div>
-       
-            </div>
-
-      
-          </form>
-  Welcome to the About Page
-This is where we talk about us.
     </div>
   );
 };
