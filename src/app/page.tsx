a85400e94ac647aed4a5cd8302ba8f0c9c925e54
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-11 02:32:33 -0400

    m

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -630,7 +630,7 @@ TEXT DA BRAIDR
             >
               <h2>
             GENERATE 
-                <br /> UUR BRAIDS
+                <br /> UR BRAIDS
               </h2>
             </div>
 
