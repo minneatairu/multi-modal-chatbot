c41de66cef82eeb4c85116cd8f83b5bf5c9f5655
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2024-10-11 02:40:40 -0400

    n

--- src/app/braiding/page.tsx
+++ src/app/braiding/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ImageGenerator from "@/components/ImageGenerator";
 
 const AboutPage = () => {
   return (
     <div>
+            <ImageGenerator />
   Welcome to the About Page
 This is where we talk about us.
     </div>
