5148be4479b9796444e662672793843870d6d2f6
Minne Atairu <79331776+minneatairu@users.noreply.github.com>
2025-02-27 15:40:48 -0500

    g

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -33,7 +33,7 @@ export default function Modal({ isOpen, onClose }: ModalProps) {
               fermentum, urna mauris blandit sapien, non aliquam arcu quam in
               turpis. Sed ultrices turpis vitae nibh ullamcorper, nec consequat
               nisl aliquam. Curabitur vel cursus lorem, ac blandit dui. Integer
-              aliquet sapien sit amet libero aliquam, ut mollis lorem interdum.
+              aliquet sapien sit amet glibero aliquam, ut mollis lorem interdum.
               Mauris a sem ut ante sollicitudin sollicitudin nec in lectus. Donec
               volutpat, odio id consequat fermentum, odio justo posuere ex, nec
               imperdiet lorem nisi vel magna. Quisque euismod, ligula ut lacinia
